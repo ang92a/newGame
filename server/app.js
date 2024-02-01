47e5e9216aa66583543ae2bd0debb5cebc6026f4
SSSamedov <65563378+Raven186@users.noreply.github.com>
2024-02-01 16:57:51 +0300

    Baza

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const indexRouter = require("./routes/index.routes");
+const { verifyAccessToken } = require("./middleware/verifyJWT");
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: "true" }));
+app.use(express.json());
+app.use(verifyAccessToken);
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is working on ${PORT} port`);
+});
