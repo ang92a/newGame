6f639e8af758229a35a797993c64066504a656b9
natalia <ang_92@list.ru>
2024-02-02 09:32:28 +0300

    redux auth

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,31 +1,55 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useAppDispatch, type RootState } from '../redux/store';
 import style from './style/Navbar.module.css';
 
 const Navbar = (): JSX.Element => {
+  const user = useSelector((store: RootState) => store.auth.auth);
+
+  // const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = (): void => {
+    fetch('/api/auth/logout')
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: '/auth/logout' });
+        // navigate('/');
+      })
+      .catch(console.log);
+  };
+
   return (
     <div className={style.container}>
       <ul className={style.navbarList}>
-        <li>
-          <NavLink className={style.navLink} to="/">
-            Привет,
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.navLink} to="/auth/sign-in">
-            Вход
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.navLink} to="/auth/sign-up">
-            Регистрация
-          </NavLink>
-        </li>
-        <li>
-          <NavLink className={style.navLink} to="/logout">
-            Выход
-          </NavLink>
-        </li>
+        {user ? (
+          <>
+            <li>
+              <NavLink className={style.navLink} to="/">
+                Привет, {user.name}
+              </NavLink>
+            </li>
+            <li onClick={handleLogout}>
+              <NavLink className={style.navLink} to="/">
+                Выход
+              </NavLink>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink className={style.navLink} to="/auth/sign-in">
+                Вход
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={style.navLink} to="/auth/sign-up">
+                Регистрация
+              </NavLink>
+            </li>
+          </>
+        )}
       </ul>
       <Outlet />
     </div>
---
+++ client/src/type.ts
@@ -0,0 +1,11 @@
+export type Theme = {
+  id: number;
+  title: string;
+};
+
+export type User = {
+  id: number;
+  email: string;
+  password: string;
+  score: number;
+};
